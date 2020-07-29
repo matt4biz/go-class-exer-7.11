fc58bacae4d11af145a58f0772770f3dcd5f0038
Matt Holiday <matt4biz@mac.com>
2020-07-29 15:10:18 -0600

    Create the start branch

--- part1/main.go
+++ part1/main.go
@@ -23,75 +23,19 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 }
 
 func (db database) add(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	price := req.URL.Query().Get("price")
-
-	if _, ok := db[item]; ok {
-		w.WriteHeader(http.StatusBadRequest) // 404
-
-		fmt.Fprintf(w, "duplicate item: %q\n", item)
-		return
-	}
-
-	if f64, err := strconv.ParseFloat(price, 32); err != nil {
-		w.WriteHeader(http.StatusBadRequest) // 400
-
-		fmt.Fprintf(w, "invalid price: %q\n", price)
-	} else {
-		db[item] = dollars(f64)
-
-		fmt.Fprintf(w, "added %s with price %s\n", item, dollars(f64))
-	}
+	// some code here
 }
 
 func (db database) update(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	price := req.URL.Query().Get("price")
-
-	if _, ok := db[item]; !ok {
-		w.WriteHeader(http.StatusNotFound) // 404
-
-		fmt.Fprintf(w, "no such item: %q\n", item)
-		return
-	}
-
-	if f64, err := strconv.ParseFloat(price, 32); err != nil {
-		w.WriteHeader(http.StatusBadRequest) // 400
-
-		fmt.Fprintf(w, "invalid price: %q\n", price)
-	} else {
-		db[item] = dollars(f64)
-
-		fmt.Fprintf(w, "new price %s for %s\n", dollars(f64), item)
-	}
+	// some code here
 }
 
 func (db database) fetch(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-
-	if _, ok := db[item]; !ok {
-		w.WriteHeader(http.StatusNotFound) // 404
-
-		fmt.Fprintf(w, "no such item: %q\n", item)
-		return
-	}
-
-	fmt.Fprintf(w, "item %s has price %s\n", item, db[item])
+	// some code here
 }
 
 func (db database) drop(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-
-	if _, ok := db[item]; !ok {
-		w.WriteHeader(http.StatusNotFound) // 404
-
-		fmt.Fprintf(w, "no such item: %q\n", item)
-		return
-	}
-
-	delete(db, item)
-
-	fmt.Fprintf(w, "dropped %s\n", item)
+	// some code here
 }
 
 func main() {
